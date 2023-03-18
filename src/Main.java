5713c1d4e2c1e82aa60558ea30c11de49197a6c7
Antonov333 <antonovsergei333@yahoo.com>
2023-03-18 11:18:23 +0300

    Prepared blank project for Webinar

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
+    static String liveDate = "18.03.2023";
+
     public static void main(String[] args) {
-        System.out.println("Live with Coach problem solving");
+        printTitle();
+        task_();
+
+        printBottomMessage();
     }
+
+    public static void printTitle() {
+        System.out.println("Решение задач, поставленных наставником на вебинаре " + liveDate);
+        System.out.println(" ");
+    }
+
+    public static void task_() {
+        System.out.println("Problem #_");
+        System.out.println("Solution to be provided soon");
+        System.out.println("");
+    }
+
+
+    public static void printBottomMessage() {
+        String status = "в процессе выполнения..." ;
+        System.out.println("Решение задач, поставленных наставником на вебинаре " + liveDate + ", " + status);
+        System.out.println(" ");
+    }
+
 }
